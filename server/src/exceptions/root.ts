72ac2e78bea898d9acbc230b00181656600ad97c
Khushi Gupta <kg7460502@gmail.com>
2026-03-17 21:45:48 +0530

    remove env

---
+++ server/src/exceptions/root.ts
@@ -0,0 +1,15 @@
+// message, status code, error code, errors
+export class HttpException extends Error {
+    message: string;
+    errorCode: any;
+    statusCode: number;
+    errors: any;
+
+    constructor(message: string, errorCode: any, statusCode: number, errors: any) {
+        super(message);
+        this.message = message;
+        this.errorCode = errorCode;
+        this.statusCode = statusCode;
+        this.errors = errors;
+    }
+}
